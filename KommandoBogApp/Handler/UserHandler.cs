fd01fc0ac2e2cc0dc94ece70c9b71684f5341c2d
Frederik Wulff Pedersen <fred957b@edu.easj.dk>
2018-05-04 10:02:49 +0200

    VM og Handler + models

---
+++ KommandoBogApp/Handler/UserHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KommandoBogApp.Model;
+using KommandoBogApp.ViewModel;
+
+namespace KommandoBogApp.Handler
+{
+    public class UserHandler
+    {
+        public UserViewModel UserVM { get; set; }
+
+        public UserHandler(UserViewModel uSW)
+        {
+            UserVM = uSW;
+        }
+
+        public void CreateUser(string maNummer,string navn, string tlf, string adresse, string email)
+        {
+            if (UserVM.Type == "Admin")
+            {
+                UserVM.UserCatalogSingleton.AddUser(new Admin(maNummer,navn,tlf,adresse,email));
+            }
+            else if (UserVM.Type == "Leader")
+            {
+                UserVM.UserCatalogSingleton.AddUser(new Leader(maNummer,navn,tlf,adresse,email));
+            }
+            else if (UserVM.Type == "Regular")
+            {
+                UserVM.UserCatalogSingleton.AddUser(new Regular(maNummer,navn,tlf,adresse,email));
+            }
+            
+        }
+    }
+}
---
+++ KommandoBogApp/ViewModel/UserViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using KommandoBogApp.Handler;
+using KommandoBogApp.Singleton;
+
+namespace KommandoBogApp.ViewModel
+{
+    public class UserViewModel
+    {
+
+
+        public UserCatalogSingleton UserCatalogSingleton { get; set; }
+        public string ViewMaNr { get; set; }
+        public string ViewNavn { get; set; }
+        public string ViewTlf { get; set; }
+        public string ViewAdresse { get; set; }
+        public string ViewEmail { get; set; }
+        public string Type { get; set; }
+
+
+        public UserHandler Handler { get; set; }
+
+
+
+
+       
+        public UserViewModel()
+        {
+            UserCatalogSingleton = UserCatalogSingleton.Instance;
+            Handler = new UserHandler(this);
+        }
+    }
+}
