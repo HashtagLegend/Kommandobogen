23f11d4743db6d41327d9695f0ec4b18bde72b33
Jacob Worm Karsberg <jaco6134@edu.easj.dk>
2018-05-07 12:33:45 +0200

    Forside

--- KommandoBogApp/App.xaml.cs
+++ KommandoBogApp/App.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using KommandoBogApp.View;
 
 namespace KommandoBogApp
 {
@@ -66,7 +67,7 @@ namespace KommandoBogApp
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                    rootFrame.Navigate(typeof(Frontpage_View), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
