2efb7f1b18d555b65a3b9b152ac1b08356e72df8
Patrick Thomas Ørum <patr421b@edu.easj.dk>
2018-05-11 11:59:47 +0200

    Added ICommands - create and delete user

--- KommandoBogApp/ViewModel/UserViewModel.cs
+++ KommandoBogApp/ViewModel/UserViewModel.cs
@@ -1,19 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using KommandoBogApp.Annotations;
 using KommandoBogApp.Handler;
 using KommandoBogApp.Model;
+using KommandoBogApp.RelayCommands;
 using KommandoBogApp.Singleton;
 
 namespace KommandoBogApp.ViewModel
 {
     public class UserViewModel
     {
-
-
+        
         public UserCatalogSingleton UserCatalogSingleton { get; set; }
         public string ViewMaNr { get; set; }
         public string ViewNavn { get; set; }
@@ -23,18 +27,32 @@ namespace KommandoBogApp.ViewModel
         public string Type { get; set; }
         public Afdeling Afdeling { get; set; }
         public User SelectedUser { get; set; }
-
-
-
-        public UserHandler Handler { get; set; }
-
-       
+        public UserHandler UserHandler { get; set; }
+        
         public UserViewModel()
         {
             UserCatalogSingleton = UserCatalogSingleton.Instance;
-            Handler = new UserHandler(this);
+            UserHandler = new UserHandler(this);
         }
 
+        #region ICommands
+
+        private ICommand _createUser;
+        private ICommand _deleteUser;
+
+        public ICommand CreateUserCommand
+        {
+            get { return _createUser ?? (_createUser = new RelayCommand(UserHandler.CreateUser)); }
+            set { _createUser = value; }
+        }
+
+        public ICommand DeleteUserCommand
+        {
+            get { return _deleteUser ?? (_deleteUser = new RelayCommand(UserHandler.DeleteUser)); }
+            set { _deleteUser = value; }
+        }
+
+        #endregion
         
     }
 }
