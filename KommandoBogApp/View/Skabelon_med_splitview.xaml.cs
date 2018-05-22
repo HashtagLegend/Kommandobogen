5bea2d221b609a0631b0f42ae617feb31099093b
Steffen Larsen <stef3998@edu.easj.dk>
2018-05-22 10:30:46 +0200

    Opdatering

--- KommandoBogApp/View/Skabelon_med_splitview.xaml.cs
+++ KommandoBogApp/View/Skabelon_med_splitview.xaml.cs
@@ -20,9 +20,9 @@ namespace KommandoBogApp.View
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class SkabelonTilViews : Page
+    public sealed partial class Skabelon_med_splitview : Page
     {
-        public SkabelonTilViews()
+        public Skabelon_med_splitview()
         {
             this.InitializeComponent();
         }
