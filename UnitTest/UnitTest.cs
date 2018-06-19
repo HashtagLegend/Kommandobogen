6598ed39c87ee537d9f33fe692e72a4948cb1386
Frederik Wulff Pedersen <fred957b@edu.easj.dk>
2018-06-19 10:33:39 +0200

    test

---
+++ UnitTest/UnitTest.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using KommandoBogApp.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestAfdelingToList()
+        {
+            Afdeling afdeling = new Afdeling("Test Afdeling", 3);
+
+            User user = new User("12345","Frederik","42489902","Helsingevej 68","fwpdanmark@hotmail.com","123");
+
+            afdeling.AddUserToList(user);
+
+            CollectionAssert.Contains(afdeling.AfdelingList,user);
+
+
+        }
+    }
+}
